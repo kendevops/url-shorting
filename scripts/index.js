65d7adb78b1fa275df738744ef305364a6f529cf
Ahuoyiza <sophieoyiza@gmail.com>
2021-01-07 22:31:03 +0100

    nav toggle function

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+//navigation
+
+function toggle() {
+    var navbar = document.getElementById("navMobile");
+    if (navbar.style.display === "flex") {
+      navbar.style.display = "none";
+    } else {
+      navbar.style.display = "flex";
+    }
+  }
