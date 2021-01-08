c56ac1a1f0710dda7f195a53c1fb304850e97c83
Ahuoyiza <sophieoyiza@gmail.com>
2021-01-08 14:23:47 +0100

    url validation

--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,53 @@ function toggle() {
       navbar.style.display = "flex";
     }
   }
+
+  const  testApi = async ()=> {
+    try{
+  let response = await fetch("https://api.shrtco.de/v2/" );
+  console.log(response)
+  let data = await response.json()
+  return data;
+   }catch (error){
+        console.log(error)
+    }
+  
+}
+
+testApi()
+  .then(data => console.log(data),{ METHOd  :  "POST"}); 
+
+
+  // get and store the url entered by user
+
+  //validate  url
+
+  const linkFunction = ()=> {
+    let url = document.getElementById("urlInput").value;
+
+   //let isValid = validUrl(url);
+    
+   console.log(validUrl(url));
+    
+    }
+    let validUrl = (url)=> {
+        let check = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+        if(!check.test(url)){
+            document.getElementById("errMsg").innerHTML = "Please add a link"
+        }else{
+            document.getElementById("errMsg").innerHTML = ""
+        }
+        
+        return console.log(url);
+        }
+    
+
+  
+  
+
+  // send the url as a post request to the api
+  // get the shortened url from the api
+  // render the link in an a tag
+  // store the link in local storage
+  // copy the link onclick
+  
